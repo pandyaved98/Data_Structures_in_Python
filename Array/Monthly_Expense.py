c2d1440336651eb601134f04b25208519d471784
Sabine Wren <pandyaved1999@gmail.com>
2021-01-04 23:56:55 +0530

    Create Monthly_Expense.py

---
+++ Array/Monthly_Expense.py
@@ -0,0 +1,9 @@
+# Let's make a list first for 6 month's expenses.
+# January - 3000 $
+# February - 2200 $
+# March - 2100 $
+# April - 2500 $
+# May - 1800 $
+# June - 2000 $
+# Let's define an array for the list given above by name exps
+exps = []
